555b844365516eca2db497f2b5ee7263e3efbb55
seveniruby <seveniruby@gmail.com>
2020-05-28 21:07:03 +0800

    po implement

--- main_page.py
+++ main_page.py
@@ -1,6 +1,23 @@
+from appium import webdriver
+
 from search_page import SearchPage
 
 
 class MainPage:
+    def __init__(self):
+        caps = {}
+        caps["platformName"] = "android"
+        caps["deviceName"] = "hogwarts"
+        caps["appPackage"] = "com.xueqiu.android"
+        caps["appActivity"] = ".view.WelcomeActivityAlias"
+        caps['unicodeKeyboard'] = True
+        caps['resetKeyboard'] = True
+        caps['autoGrantPermissions'] = True
+        caps['noReset'] = True
+
+        self.driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", caps)
+        self.driver.implicitly_wait(10)
+        # self.driver.find_element_by_id("tv_agree").click()
     def to_search(self):
-        return SearchPage()
+        self.driver.find_element_by_id("home_search").click()
+        return SearchPage(self.driver)
--- search_page.py
+++ search_page.py
@@ -1,8 +1,12 @@
-
 class SearchPage:
+    def __init__(self, driver):
+        self.driver = driver
+
     def search(self, keyword):
+        self.driver.find_element_by_id("search_input_text").send_keys(keyword)
+        self.driver.find_element_by_id('name').click()
         return self
 
     def get_price(self, keyword=None):
-        return 100
-
+        price = self.driver.find_element_by_id('current_price').text
+        return float(price)
