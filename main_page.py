9e200f7d3ec0cf01761d2ade48f5d503adbeaeae
seveniruby <seveniruby@gmail.com>
2020-05-28 21:02:06 +0800

    po desigin and testcase design

---
+++ main_page.py
@@ -0,0 +1,6 @@
+from search_page import SearchPage
+
+
+class MainPage:
+    def to_search(self):
+        return SearchPage()
---
+++ search_page.py
@@ -0,0 +1,8 @@
+
+class SearchPage:
+    def search(self, keyword):
+        return self
+
+    def get_price(self, keyword=None):
+        return 100
+
